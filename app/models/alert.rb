54daae5d7f8874cdb0f2ccc2a99d1c6390545820
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-09-09 15:11:14 +0200

    compute search now part of alert model

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -13,4 +13,40 @@ class Alert < ApplicationRecord
   def array_from_mentionned_accounts
     self.mentionned_accounts.split(',')
   end
+
+  def keyword_creation
+    if self.exact_keyword
+      '"' + "#{self.keyword}" + '"'
+    else
+      "#{self.keyword}"
+    end
+  end
+
+  def excluded_keywords_creation
+    result = ""
+    self.array_from_keywords_excluded.each do | keyword |
+      result += "-#{keyword.strip} "
+    end
+    result.strip
+  end
+
+  def hashtags_creation
+    result = ""
+    self.array_from_hashtags.each do | hashtag |
+      result += "#{hashtag.strip} "
+    end
+    result.strip
+  end
+
+  def retweets_creation
+    if self.retweets_included
+      ""
+    else
+      "-rt -RT"
+    end
+  end
+
+  def compute_search
+    "#{self.keyword_creation} #{self.excluded_keywords_creation} #{self.hashtags_creation} #{self.retweets_creation}"
+  end
 end
